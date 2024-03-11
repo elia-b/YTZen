e37c156d765116e4a339738538bbe7083ba6bfd4
elia-b <elia.boscaini@gmail.com>
2024-03-11 21:46:55 +0100

    Add Browser plugin

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <img
+    width="250"
+    src="icon.svg"
+    alt="YouTube Zen - personal Browser plug-in to minimize distractions on YouTube."
+  />
+</p>
+
+# YouTube Zen
+
+This Browser plug-in is meant for personal use.
+Feel anyway free to leave comments and suggestions.
+
+Features:
+
+- Redirect any YouTube home page request to your subscriptions feed
+- Minor advartisment manipulation on YouTube
---
+++ app.js
@@ -0,0 +1,65 @@
+function executeOnLoad() {
+  const targetNode = document.querySelector("ytd-app");
+  if (targetNode) {
+    const config = {
+      childList: true,
+      subtree: true,
+    };
+    let observer = new MutationObserver(polishYoutube);
+    observer.observe(targetNode, config);
+    polishYoutube();
+  } else {
+    setTimeout(function () {
+      executeOnLoad();
+    }, 100);
+  }
+}
+
+function removeAdSlots() {
+  console.log("Removing all ad slots");
+  const adTag = ["ytd-ad-slot-renderer", "ytd-action-companion-ad-renderer"];
+  adTag.forEach((element) => {
+    const adSlots = document.querySelectorAll("ytd-ad-slot-renderer");
+
+    adSlots.forEach((adSlot) => {
+      adSlot.parentNode.removeChild(adSlot);
+    });
+  });
+}
+
+function removeShorts() {
+  console.log("Removing all shorts");
+  const shorts = document.querySelectorAll('[is-shorts=""]');
+
+  shorts.forEach((short) => {
+    short.parentNode.removeChild(short);
+  });
+}
+
+function polishYoutube() {
+  removeAdSlots();
+  removeShorts();
+}
+
+function redirectToSubscriptions() {
+  if (
+    window.location.hostname === "www.youtube.com" &&
+    window.location.pathname === "/"
+  ) {
+    window.location.href = "https://www.youtube.com/feed/subscriptions";
+  }
+}
+
+let previousUrl = "";
+
+let urlObserver = new MutationObserver(function (mutations) {
+  if (location.href !== previousUrl) {
+    previousUrl = location.href;
+    console.log(`URL data changed to ${location.href}`);
+    redirectToSubscriptions();
+    executeOnLoad();
+  }
+});
+
+const urlConfig = { attributes: true, childList: true, subtree: true };
+urlObserver.observe(document, urlConfig);
